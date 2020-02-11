d18fb3033333ace17e96cfa3f6697061aad87e97
Leonardo Almeida <leo@leleo.com.br>
2020-02-11 13:50:44 +0100

    fix mobile ontouchstart

--- day02/js/script.js
+++ day02/js/script.js
@@ -1,11 +1,25 @@
+var button = document.querySelector("input");
+
+button.ontouchstart = myManBg;
+
+button.addEventListener(ontouchstart, function() {
+    document.body.style.backgroundImage = "url('./myman.jpg')";
+})
+
+button.addEventListener(ontouchstart, function() {
+    document.body.style.backgroundImage = 'none';
+})
+
 function play() {
-    var audio = new Audio('./myman.mp4');
-    audio.play();
+
 
 }
 
 function myManBg() {
-    document.body.style.backgroundImage = "url('./myman.jpg ')";
+    document.body.style.backgroundImage = "url('./myman.jpg')";
+    var audio = new Audio('./myman.mp4');
+    audio.play();
+    console.log('4231')
 
 }
 
